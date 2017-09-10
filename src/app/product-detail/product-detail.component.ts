a8c2ed90f4849180b14d403b8dc1392cd3cc82ce
ElinorSu <wosuting@126.com>
2017-09-10 14:48:49 +0800

    sixth-steps

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -12,6 +12,9 @@ export class ProductDetailComponent implements OnInit {
   productId: number;
   product: Product;
   comments: Comment[];
+  newRating: number = 5;
+  newComment: string = '';
+  isCommentHidden = true;
   constructor(private routeInfo: ActivatedRoute,
               private productService: ProductService) { }
 
@@ -21,6 +24,15 @@ export class ProductDetailComponent implements OnInit {
     // this.productTitle = this.routeInfo.snapshot.params["prodTitle"];
     this.product = this.productService.getProduct(this.productId);
     this.comments = this.productService.getCommentsForProductId(this.productId);
+  }
+  addComment() {
+    let comment = new Comment(0, this.productId, new Date().toISOString(), 'someone', this.newRating, this.newComment);
+    this.comments.unshift(comment);
+    let sum = this.comments.reduce((sum, comment) => sum + comment.rating, 0);
+    this.product.rating = sum/ this.comments.length;
+    this.newComment = null;
+    this.newRating = 5;
+    this.isCommentHidden = true;
   }
 
 }
